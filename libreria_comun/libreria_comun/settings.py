94b3fbf82b367cd784545be2edbefc93911260c6
luciabalbas <luciabalrub@gmail.com>
2025-05-08 20:45:02 +0200

    Instalando la app core

--- libreria_comun/libreria_comun/settings.py
+++ libreria_comun/libreria_comun/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # mis apps
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
