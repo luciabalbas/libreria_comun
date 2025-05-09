3806925cd8afc8504d65329861ffb3f8fe3e4842
luciabalbas <luciabalrub@gmail.com>
2025-05-09 19:59:56 +0200

    Mejorando book_list: vista, estilos y pagination

--- libreria_comun/books/views.py
+++ libreria_comun/books/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Book
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 class BookListView(ListView):
     """ Vista de la lista de Libros """
     model = Book
+    context_object_name = 'book_list'
+    paginate_by = 4
     template_name = 'books/book_list.html'
 
 class BookDetailView(DetailView):
