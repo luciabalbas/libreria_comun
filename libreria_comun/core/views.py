a0d212ffe77f8abff8c48e1bedce1c59cf40b405
luciabalbas <luciabalrub@gmail.com>
2025-05-12 20:21:31 +0200

    Añadiendo vista de últimos libros añadidos en la página principal

--- libreria_comun/core/views.py
+++ libreria_comun/core/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views.generic.base import TemplateView
+from books.models import Book
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'core/home.html'
-    
+    def get(self, request):
+        last_books = get_list_or_404(Book.objects.order_by('-created')[:4])
+        return render (request, 'core/home.html', {'last_books': last_books})
 
 class SamplePageView(TemplateView):
     template_name = 'core/sample.html'
